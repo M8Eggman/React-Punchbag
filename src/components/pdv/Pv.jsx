2336fa113dc7062395b016dc91be837dc7f10851
GoNEAR <cagatay.morgul@outlook.com>
2025-05-12 11:24:40 +0200

    structure component Pv

--- src/components/pdv/Pv.jsx
+++ src/components/pdv/Pv.jsx
@@ -3,7 +3,10 @@ import "./Pv.css";
 function Pv() {
   return (
     <>
-   
+      <div className="pvMax">
+        <div className="pvActuel"></div>
+        <p>100 PV</p>
+      </div>
     </>
   );
 }
