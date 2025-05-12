be8fa0240a734ff4dbefb347c3dde9c001fd2132
GoNEAR <cagatay.morgul@outlook.com>
2025-05-12 11:39:01 +0200

    fonctionnalité pv

--- src/components/pdv/Pv.jsx
+++ src/components/pdv/Pv.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./Pv.css";
 
 function Pv() {
+  const [pv, setPv] = useState(0);
   return (
     <>
       <div className="pvMax">
-        <div className="pvActuel"></div>
+        <div className="pvActuel" style={{ width: pv + "vw" }}></div>
       </div>
     </>
   );
